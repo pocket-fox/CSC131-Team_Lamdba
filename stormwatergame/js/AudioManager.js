f6520122922b1ced91336957e8a3c8f70a1b81d1
Brittnee McDonald <brittneemcdonald95@gmail.com>
2025-10-08 22:23:43 -0700

    Changes v2
    Added start scene that shows control. Just a demo, will update later.
    
    Added m to mute the game up until game choosing, will add to the rest later.
    
    Added a avria-live screen recorder, only works on the start page as a demo, will update later.

---
+++ stormwatergame/js/AudioManager.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var AudioManager = {
+  init: function () {
+    this.indexAB = 0;
+    this.indexC = 1;
+  },
+
+  toggleMusic: function (screen) {
+    Game.sound.mute = !Game.sound.mute;
+    if (Game.sound.mute) {
+      // muted, show canceled sound button
+      screen.muteButton.setFrames(2, 2, 3);
+      this.indexAB = 2;
+      this.indexC = 3;
+    } else {
+      // unmuted, show mute sound button
+      screen.muteButton.setFrames(0, 0, 1);
+      this.indexAB = 0;
+      this.indexC = 1;
+    }
+  },
+
+  playSong: function (title, state) {
+    // Make sure to not replay same song
+    if (!this.currentTitle || this.currentTitle != title) {
+      this.currentTitle = title;
+      if (this.currentSong != null) this.currentSong.stop();
+      this.currentSong = state.add.audio(title, 0.3);
+      this.currentSong.loopFull();
+    }
+  },
+
+  playSound: function (title, state) {
+    this.sound = state.add.audio(title, 0.25);
+    this.sound.play();
+  },
+};
+
+var muteButtonActions = {
+  onClick: function () {
+    AudioManager.toggleMusic(this);
+  },
+};
+
+function createMuteButton(scene) {
+  var indexAB = AudioManager.indexAB;
+  var indexC = AudioManager.indexC;
+  scene.muteButton = scene.add.button(
+    0.892 * WIDTH,
+    0.02 * HEIGHT,
+    "button_sound",
+    muteButtonActions.onClick,
+    scene,
+    indexAB,
+    indexAB,
+    indexC
+  );
+  scene.muteButton.scale.setTo(0.75);
+  return scene.muteButton;
+}
+
+function createMuteButtonPos(scene, x, y) {
+  var indexAB = AudioManager.indexAB;
+  var indexC = AudioManager.indexC;
+  scene.muteButton = scene.add.button(
+    x * WIDTH,
+    y * HEIGHT,
+    "button_sound",
+    muteButtonActions.onClick,
+    scene,
+    indexAB,
+    indexAB,
+    indexC
+  );
+  scene.muteButton.scale.setTo(0.75);
+  return scene.muteButton;
+}
