074265ad7d15b760bad3ccf8a89096df58c225ca
pocket-fox <connor.lecluse@pm.me>
2025-11-19 14:12:16 -0800

    Quick Test

--- public/js/scenes/Title.js
+++ public/js/scenes/Title.js
@@ -53,14 +53,14 @@ var TitleState = {
 
     //*****************************************************************
       // captures enter key
-      // this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-      // this.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+      this.spaceKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+      this.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
 
-      // // debug message + button click (open in browser, press f12)
-      // this.spaceKey.onDown.add(function () {
-      //     console.log("Enter pressed on Title screen!");
-      //     this.playButtonActions.onClick.call(this);
-      // }, this);
+      // debug message + button click (open in browser, press f12)
+      this.spaceKey.onDown.add(function () {
+          console.log("Enter pressed on Title screen!");
+          this.playButtonActions.onClick.call(this);
+      }, this);
 
       // captures m key
       this.mKey = this.input.keyboard.addKey(Phaser.Keyboard.M);
